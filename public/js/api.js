0a2d51592470d453366a268aa6802f38939fc900
kyleconyers <conyersky@gmail.com>
2018-12-04 20:31:06 -0800

    commit to pull background

--- public/js/api.js
+++ public/js/api.js
@@ -4,12 +4,148 @@ const urlStringRenewables = "http://api.eia.gov/category/?api_key=" + APIkey + "
 const urlStringElectricity = `http://api.eia.gov/series/?api_key=${APIkey}&series_id=INTL.2-12-WORL-BKWH.A`;
 const urlStringFFuel = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.28-12-WORL-BKWH.A";
 const urlStringCoal = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.29-12-WORL-BKWH.A";
-console.log("loading");
-// console.log(urlString);
+
+
+
+const urlStringAusCoal = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-AUS-QBTU.A";
+
+var isoArray = ["AUS", "BRA", "CHN", "COD", "FRA", "IND", "RUS", "USA"]
 var electricArray = [];
 var coalArray = [];
 var ffuelArray = [];
+var AusCoalArray = [];
 var worldCreated;
+var totalCoalArrayList = [];
+var totalNaturalGasList = [];
+ 
+var usaTotal = [];
+
+
+const coalArr = [];
+const naturalGasArr = [];
+var UsaArr = [];
+
+
+const urlCountriesCoal = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-" + "USA" + "-QBTU.A"
+const urlCountriesNaturalGas = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4413-2-" + "USA" + "-QBTU.A";
+const urlCountriesPetroleum = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.5-2-" + "USA" + "-QBTU.A";
+const urlCountriesJetFuel = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.63-2-" + "USA" + "-QBTU.A";
+//renewable
+const urlCountriesNuclear = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.27-12-" + "USA" + "-BKWH.A";
+const urlCountriesSolar = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.116-12-" + "USA" + "-BKWH.A";
+const urlCountriesWind = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.37-12-" + "USA" + "-QBTU.A";
+const urlCountriesBiofuels = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.79-2-" + "USA" + "-QBTU.A";
+const urlStringAUSCoal = "http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-" + "AUS" + "-QBTU.A";
+const usaTotalApi = [urlCountriesCoal, urlCountriesNaturalGas, urlCountriesPetroleum, urlCountriesJetFuel, urlCountriesNuclear, urlCountriesSolar, urlCountriesWind, urlCountriesBiofuels, urlStringAUSCoal];
+
+
+function fetchTotalUsa(url) {
+    return fetch(url).then(function (res) {
+        return res.json()
+    }).then(function (dataTotalUsa) {
+        // console.log("data Coal here ", dataCoal)
+        // console.log(dataTotalCoal)
+        totalUsaArray = dataTotalUsa['series'][0]['data'];
+        // console.log("Coal array is heeeere", coalArray);
+        // console.log("Total coal", totalCoalArray);
+        UsaArr.push(totalUsaArray)
+        // console.log("test", totalUsaArray);
+        return totalUsaArray;
+        
+    });
+
+};
+// fetchTotalCoal();
+var totalUsaArray = usaTotalApi.map(function(url){
+    // console.log("http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-" + element + "-QBTU.A");
+    return fetchTotalUsa(url)
+})
+
+Promise.all(totalUsaArray).then(function(values) {
+    console.log('USA', values)
+})
+
+
+
+
+
+
+
+
+
+
+function fetchTotalCoal(url) {
+    return fetch(url).then(function (res) {
+        return res.json()
+    }).then(function (dataTotalCoal) {
+        // console.log("data Coal here ", dataCoal)
+        // console.log(dataTotalCoal)
+        totalCoalArray = dataTotalCoal['series'][0]['data'];
+        // console.log("Coal array is heeeere", coalArray);
+        // console.log("Total coal", totalCoalArray);
+        coalArr.push(totalCoalArray)
+        return totalCoalArray;
+    });
+
+};
+// fetchTotalCoal();
+var totalCoalArray = isoArray.map(function(element){
+    // console.log("http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-" + element + "-QBTU.A");
+    return fetchTotalCoal("http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-" + element + "-QBTU.A")
+})
+
+Promise.all(totalCoalArray).then(function(values) {
+    console.log('coal values', values)
+})
+
+function fetchTotalNaturalGas(url) {
+    return fetch(url).then(function (res) {
+        return res.json()
+    }).then(function (dataTotalNaturalGas) {
+        // console.log("data Coal here ", dataCoal)
+        // console.log(dataTotalCoal)
+        totalNaturalGasArray = dataTotalNaturalGas['series'][0]['data'];
+        // console.log("Coal array is heeeere", coalArray);
+        // console.log("Total coal", totalCoalArray);
+        naturalGasArr.push(totalNaturalGasArray)
+        return totalNaturalGasArray;
+    });
+
+};
+// fetchTotalCoal();
+var totalNaturalGasArray = isoArray.map(function(element){
+    // console.log("http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4411-2-" + element + "-QBTU.A");
+    return fetchTotalNaturalGas("http://api.eia.gov/series/?api_key=" + APIkey + "&series_id=INTL.4413-2-" + element + "-QBTU.A")
+})
+
+Promise.all(totalNaturalGasArray).then(function(values) {
+    console.log('natural gas values', values)
+})
+
+// console.log(totalCoalArray)
+// console.log('arr', arr)
+// console.log(totalCoalArray)
+// for(let i = 0; i < totalCoalArray.length; i++) {
+//    console.log(totalCoalArray[i])
+
+//    function fetchTotalCoal() {
+//     return fetch(totalCoalArray[i]).then(function (res) {
+//         return res.json()
+//     }).then(function (dataTotalCoal) {
+//         // console.log("data Coal here ", dataCoal)
+//         // console.log(dataTotalCoal)
+//         totalCoalArray = dataTotalCoal['series'][0]['data'];
+//         // console.log("Coal array is heeeere", coalArray);
+//         console.log("Total coal", totalCoalArray);
+//         return totalCoalArray;
+//     });
+
+// };
+// fetchTotalCoal();
+// }
+
+
+
 
 // Scope issue if we don't return the entire function body. The lesson learned is not to return up to the body but to the entire body. 
 function fetchElec() {
@@ -21,6 +157,7 @@ function fetchElec() {
         // console.log("It's meeeeeeee", electricArray);
         return electricArray;
     })
+    
 }
 
 function fetchFFuel() {
@@ -45,8 +182,27 @@ function fetchCoal() {
 
 };
 
+function fetchAusCoal() {
+    return fetch(urlStringAusCoal).then(function (res) {
+        return res.json()
+    }).then(function (dataAusCoal) {
+        // console.log("data Coal here ", dataCoal)
+        // console.log(dataAusCoal)
+        AusCoalArray = dataAusCoal['series'][0]['data'];
+        // console.log("Coal array is heeeere", coalArray);
+        // console.log("Aus coal", AusCoalArray);
+        return AusCoalArray;
+    });
 
-console.log('function check ', fetchCoal());
+};
+fetchAusCoal();
+
+
+// console.log(countryCoalArray)
+
+
+
+// console.log('function check ', fetchCoal());
 // async function fetchAll() {
 //     await fetchCoal();
 //     await fetchElec();
@@ -58,7 +214,7 @@ console.log('function check ', fetchCoal());
 
 Promise.all([fetchCoal(), fetchFFuel(), fetchElec()]).then(function (data) {
     // console.log(data);
-    console.log(electricArray);
+    // console.log(electricArray);
     // console.log(coalArray);
     // console.log(ffuelArray);
    
@@ -68,7 +224,7 @@ Promise.all([fetchCoal(), fetchFFuel(), fetchElec()]).then(function (data) {
     ]
 
     })
-    console.log(chartData)
+    // console.log(chartData)
 
     var worldAggArr = data[0].map(obj => {
         var newObjectIwant = {};
@@ -86,7 +242,7 @@ Promise.all([fetchCoal(), fetchFFuel(), fetchElec()]).then(function (data) {
     // console.log(worldAggArr);
  
     function createworld() {
-        console.log(coalArray)
+        // console.log(coalArray)
         var chartData = electricArray.map(function(year, i){
             return [year[0], year[1], coalArray[i][1],
                                       ffuelArray[i][1]
@@ -110,7 +266,7 @@ Promise.all([fetchCoal(), fetchFFuel(), fetchElec()]).then(function (data) {
         // });
         // makeGraph(worldAggArr);
         // console.log("world agg object here ", worldAggArr)
-        console.log(finalData, "envoked")
+        // console.log(finalData, "envoked")
         return finalData;
     };
     worldCreated = createworld();
@@ -149,3 +305,252 @@ function createWorld() {
     console.log("world agg object here ", worldAggArr)
 };
 
+
+// AFG
+// ALA
+// ALB
+// DZA
+// ASM
+// AND
+// AGO
+// AIA
+// ATA
+// ATG
+// ARG
+// ARM
+// ABW
+// AUS
+// AUT
+// AZE
+// BHS
+// BHR
+// BGD
+// BRB
+// BLR
+// BEL
+// BLZ
+// BEN
+// BMU
+// BTN
+// BOL
+// BES
+// BIH
+// BWA
+// BVT
+// BRA
+// IOT
+// VGB
+// BRN
+// BGR
+// BFA
+// BDI
+// CPV
+// KHM
+// CMR
+// CAN
+// CYM
+// CAF
+// TCD
+// CHL
+// CHN
+// HKG
+// MAC
+// CXR
+// CCK
+// COL
+// COM
+// COG
+// COK
+// CRI
+// CIV
+// HRV
+// CUB
+// CUW
+// CYP
+// CZE
+// PRK
+// COD
+// DNK
+// DJI
+// DMA
+// DOM
+// ECU
+// EGY
+// SLV
+// GNQ
+// ERI
+// EST
+// SWZ
+// ETH
+// FLK
+// FRO
+// FJI
+// FIN
+// FRA
+// GUF
+// PYF
+// ATF
+// GAB
+// GMB
+// GEO
+// DEU
+// GHA
+// GIB
+// GRC
+// GRL
+// GRD
+// GLP
+// GUM
+// GTM
+// GGY
+// GIN
+// GNB
+// GUY
+// HTI
+// HMD
+// VAT
+// HND
+// HUN
+// ISL
+// IND
+// IDN
+// IRN
+// IRQ
+// IRL
+// IMN
+// ISR
+// ITA
+// JAM
+// JPN
+// JEY
+// JOR
+// KAZ
+// KEN
+// KIR
+// KWT
+// KGZ
+// LAO
+// LVA
+// LBN
+// LSO
+// LBR
+// LBY
+// LIE
+// LTU
+// LUX
+// MDG
+// MWI
+// MYS
+// MDV
+// MLI
+// MLT
+// MHL
+// MTQ
+// MRT
+// MUS
+// MYT
+// MEX
+// FSM
+// MCO
+// MNG
+// MNE
+// MSR
+// MAR
+// MOZ
+// MMR
+// NAM
+// NRU
+// NPL
+// NLD
+// NCL
+// NZL
+// NIC
+// NER
+// NGA
+// NIU
+// NFK
+// MNP
+// NOR
+// OMN
+// PAK
+// PLW
+// PAN
+// PNG
+// PRY
+// PER
+// PHL
+// PCN
+// POL
+// PRT
+// PRI
+// QAT
+// KOR
+// MDA
+// REU
+// ROU
+// RUS
+// RWA
+// BLM
+// SHN
+// KNA
+// LCA
+// MAF
+// SPM
+// VCT
+// WSM
+// SMR
+// STP
+// SAU
+// SEN
+// SRB
+// SYC
+// SLE
+// SGP
+// SXM
+// SVK
+// SVN
+// SLB
+// SOM
+// ZAF
+// SGS
+// SSD
+// ESP
+// LKA
+// PSE
+// SDN
+// SUR
+// SJM
+// SWE
+// CHE
+// SYR
+// TJK
+// THA
+// MKD
+// TLS
+// TGO
+// TKL
+// TON
+// TTO
+// TUN
+// TUR
+// TKM
+// TCA
+// TUV
+// UGA
+// UKR
+// ARE
+// GBR
+// TZA
+// UMI
+// USA
+// VIR
+// URY
+// UZB
+// VUT
+// VEN
+// VNM
+// WLF
+// ESH
+// YEM
+// ZMB
+// ZWE
