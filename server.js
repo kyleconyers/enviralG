48ea70e95783dd008093f540e39595e3e102e8d1
thescottbob <scottmcmains@gmail.com>
2018-12-11 12:51:25 -0800

    Improved Front End Formatting

--- server.js
+++ server.js
@@ -1,46 +1,31 @@
+// Dependencies
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var path = require("path");
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-const favicon = require("express-favicon");
-var favicon = require("serve-favicon");
-var path = require("path");
-
-
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-// Static directory
-app.use(express.static("public"));
 
 // Routes
-// =============================================================
 require("./routes/htmlroutes.js")(app);
 require("./routes/author-api-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
 require("./routes/comment-api-routes.js")(app);
 
-
 // Handlebars
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     defaultLayout: "main"
-//   })
-// );
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
-// // Routes
-// require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 var syncOptions = { force: false };
 
@@ -50,19 +35,13 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-//   app.listen(PORT, function() {
-//     console.log(
-//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
+// const sequelize = new Sequelize('messageBoard', 'root', '', {
+//   dialect: 'mysql',
+//   host: 'localhost',
+//   port: 3000,
+// })
 
 // Syncing our sequelize models and then starting our Express app
-// =============================================================
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
